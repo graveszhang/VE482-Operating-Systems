74efcd06ccf7a67954d94ecbf193f346b4cd6753
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2020-09-30 00:14:54 +0800

    Delete ShanghaiAir.sh

--- lab/l3/ShanghaiAir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl http://aqicn.org/?city=Shanghai&widgetscript&size=large&id=52b39d71decf07.20261781 -o Raw
-cand=$(cat Raw | sed 's/ /\n/g' | sed -n 's~>\([0-9]*\)<\\\/td~Temp: \1~p')
-cand1=$(cat $cand | sed -n 's~\(Temp: [0-9]*\)><\\\/tr><\\\/table~\1~p')
-echo $cand1
-
