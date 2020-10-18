da4fbf51aff71087f27e9d192960c53dc4e015d9
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2020-10-18 20:28:12 +0800

    Add files via upload

---
+++ homework/h3/cmake-build-debug/CMakeFiles/h3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/h3.dir/linkedlist.c.obj"
+  "CMakeFiles/h3.dir/main.c.obj"
+  "h3.exe"
+  "h3.exe.manifest"
+  "h3.pdb"
+  "libh3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/h3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
